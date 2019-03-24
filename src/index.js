1b25568e9f967ee124677da2f0afab5ee65365f1
Anna Zankevich <annzankevich18@gmail.com>
2019-03-24 16:15:50 +0300

    add love triangle

--- src/index.js
+++ src/index.js
@@ -3,5 +3,15 @@
  * @returns number of love triangles
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
-  // your implementation
+    let link = 0;
+    preferences.map((item, i) => {
+        console.log(item, i);
+        let firstlover = item - 1;
+        let secondlover = preferences[firstlover] - 1;
+        let thirdlover = preferences[secondlover] - 1;
+        if (thirdlover === i) {
+            link++;
+        }
+    });
+    return Math.floor(link / 3);
 };
